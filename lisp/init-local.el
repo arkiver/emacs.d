91147cd7b333c1bd31be95e700b60319e0b1b066
Akshay Khole <akshay@reversehack.in>
2014-02-10 16:28:13 +0530

    Do not set lang envs

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -5,11 +5,5 @@
 (setq inferior-lisp-program "~/bin/sbcl")
 (load (expand-file-name "~/quicklisp/slime-helper.el"))
 
-(setq slime-lisp-implementations
-      '((sbcl ("~/bin/sbcl") :coding-system utf-8-unix)))
-
-(set-language-enviroment "UTF-8")
-(setq slime-net-coding-system 'utf-8-unix)
-
 ;;; provide this file
 (provide 'init-local)
